125734f0dc8cde047c246c140405251300a85bc7
Willians Nieves <williamsnieves@gmail.com>
2021-10-31 16:23:25 +0100

    feat: init gatsby site

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>About page</h1>
+    </main>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function IndexPage() {
+  return (
+    <main>
+      <h1>Hi gatsby</h1>
+    </main>
+  );
+}
